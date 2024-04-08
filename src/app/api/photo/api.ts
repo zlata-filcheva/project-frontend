99db1c514ac2241a045a8ea5fa7ff28a3194fe60
Igor Filchev <filchev.igor@gmail.com>
2024-04-08 21:41:28 +0300

    Enabling https

--- src/app/api/photo/api.ts
+++ src/app/api/photo/api.ts
@@ -2,7 +2,7 @@ import { PhotosType } from "../../types/photos.ts";
 
 export const downloadPhoto = async (): Promise<PhotosType[]> => {
   const response = await fetch(
-    "http://localhost/2sd/index.php/photo/list?limit=20",
+    "https://127.0.0.1/2sd/index.php/photos/list?limit=20",
   );
 
   return await response.json();
