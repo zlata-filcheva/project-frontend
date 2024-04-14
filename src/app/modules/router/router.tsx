b13dfb8cb4861a977c0f57d3422a5269917cdfd6
Igor Filchev <90631197+filchev-igor@users.noreply.github.com>
2024-04-14 14:56:10 +0300

    1-Login-Page (#1)

--- src/app/modules/router/router.tsx
+++ src/app/modules/router/router.tsx
@@ -1,17 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../../pages/HomePage/HomePage.tsx";
 import { PATH_NAMES } from "./routes.ts";
 import LoginPage from "../../pages/LoginPage/LoginPage";
+import SignUpPage from "../../pages/SignUpPage/SignUpPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: PATH_NAMES.homePage,
     children: [
       {
-        element: <HomePage />,
+        element: <LoginPage />,
         index: true,
       },
-      {element: <LoginPage />, path:PATH_NAMES.loginPage}
+      {
+        element: <SignUpPage />,
+        path: PATH_NAMES.signUpPage,
+      },
     ],
   },
 ]);
---
+++ src/app/pages/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import { usePageTitle } from "../../utils/usePageTitle/usePageTitle.ts";
+import "./styles.css";
+
+const SignUpPage = () => {
+  usePageTitle("Login page");
+
+  //const { data, isLoading } = usePhotoQuery();
+
+  return null;
+};
+
+export default SignUpPage;
